32a436701a5d444f3916e017f5a7a97d076becaa
THU THAO <thaodoan.2x06@gmail.com>
2022-03-28 22:18:17 +0700

    update

---
+++ DuaXe/Assets/Scripts/Go.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Go : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject LapCompleteTrig;
+    public GameObject HalfLapTrig;
+    void OnTriggerEnter(){
+        LapCompleteTrig.SetActive(true);
+        HalfLapTrig.SetActive(false);
+    }
+}
